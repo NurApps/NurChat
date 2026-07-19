fd312591f5f31e0735fad06b070aa777a66c8d67
salihhhh014 <salixsuyundikov@gmail.com>
2026-07-19 18:45:28 +0400

    fix: copy server.exe to Tauri sidecar in build-server.bat, add 127.0.0.1 to CSP

--- build-server.bat
+++ build-server.bat
@@ -6,9 +6,13 @@ pip install pyinstaller>=6.0
 
 echo [2/3] Building server.exe...
 pyinstaller server.spec --clean --noconfirm
+if errorlevel 1 exit /b %errorlevel%
 
-echo [3/3] Done!
-echo.
-echo Output: dist\server\server.exe
+echo [3/3] Copying to Tauri sidecar...
+copy /Y dist\server.exe src-tauri\binaries\server-x86_64-pc-windows-msvc.exe
+
+echo Done!
+echo   Output: dist\server.exe
+echo   Sidecar: src-tauri\binaries\server-x86_64-pc-windows-msvc.exe
 echo.
 pause
